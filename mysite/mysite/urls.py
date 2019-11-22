62bf27acb3297692876b1e13013384a152de854b
Lee Chengkai <leechengkai@gmail.com>
2019-11-22 11:01:37 +0800

    Add autoformatters for HTML and CSS

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import HttpResponseRedirect
 
 urlpatterns = [
+    path("", lambda r: HttpResponseRedirect("polls/")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
