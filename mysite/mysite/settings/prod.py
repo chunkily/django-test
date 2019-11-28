5e2159756bfe64b2fa54c16850e53eb33bce81c0
Lee Chengkai <leechengkai@gmail.com>
2019-11-28 09:24:59 +0800

    Move static files to /var/www/django/static/

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -8,7 +8,11 @@ ALLOWED_HOSTS = ["*"]
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-STATIC_ROOT = "/static"
+# URL to use when referring to static files located in STATIC_ROOT
+STATIC_URL = "/static/"
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = "/var/www/django/static/"
 
 DATABASES = {
     "default": {
