0c939cbdbbe26540b1958a78d635c98ef87ab99c
Lee Chengkai <leechengkai@gmail.com>
2019-11-29 11:11:15 +0800

    Add Django Rest Framework

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from polls import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
 
 app_name = "polls"
 urlpatterns = [
@@ -8,4 +14,5 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("api/", include(router.urls)),
 ]
